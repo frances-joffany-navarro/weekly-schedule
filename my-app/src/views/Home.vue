620b3ac6c146110410e08494fc17137a9adf74d0
Frances Joffany Navarro <francesjoffanynavarro@gmail.com>
2022-10-24 16:37:45 +0200

    added date group button with css

--- my-app/src/views/Home.vue
+++ my-app/src/views/Home.vue
@@ -1,24 +1,57 @@
 <template>
-  <div class="home text-left">
-    <div class="container">
-      <div class="row">
-        <div class="col-sm">
-          Create Schedule
-          <Form></Form>
-        </div>
-        <div class="col-sm">One of three columns</div>
-      </div>
+  <div>
+    <a class="btn btn-success" href="#" role="button">Add Schedule</a>
+
+    <div class="btn-group mt-2" role="group" aria-label="Schedule">
+      <a class="btn btn-secondary" href="#" role="button"
+        ><p class="day-label">SUN</p>
+        23</a
+      >
+      <a class="btn btn-secondary" href="#" role="button"
+        ><p class="day-label">MON</p>
+        24</a
+      >
+      <a class="btn btn-secondary" href="#" role="button"
+        ><p class="day-label">TUE</p>
+        25</a
+      >
+      <a class="btn btn-secondary" href="#" role="button"
+        ><p class="day-label">WED</p>
+        26</a
+      >
+      <a class="btn btn-secondary" href="#" role="button"
+        ><p class="day-label">THU</p>
+        27</a
+      >
+      <a class="btn btn-secondary" href="#" role="button"
+        ><p class="day-label">FRI</p>
+        28</a
+      >
+      <a class="btn btn-secondary" href="#" role="button"
+        ><p class="day-label">SAT</p>
+        30</a
+      >
     </div>
-    
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import Form from "@/components/Form.vue";
 
 export default {
   name: "Home",
-  components: { Form },
+  data() {
+    return {
+      schedules: [
+        {
+          scheduleName: "French Class",
+          scheduleTimeIn: "18:30",
+          scheduleTimeOut: "21:30",
+          scheduleDate: "24/10/2022",
+          scheduleDay: "Monday",
+        },
+      ],
+    };
+  },
 };
 </script>
